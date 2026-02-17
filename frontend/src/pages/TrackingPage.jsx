90d23fa2dd37d629eb798814013d7f10a00a4d05
Michael Mbugua <michaelmbugua777@gmail.com>
2026-02-17 18:28:24 +0300

    update tracking page to have data available to its header

--- frontend/src/pages/TrackingPage.jsx
+++ frontend/src/pages/TrackingPage.jsx
@@ -3,13 +3,13 @@ import { Header } from '../components/Header';
 import { Link } from 'react-router';
 import trackingIcon from '../assets/images/tracking-favicon.png';
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={trackingIcon} />
       <title>Tracking</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div class="tracking-page">
       <div class="order-tracking">
--- frontend/src/pages/orders/OrderDetailsGrid.jsx
+++ frontend/src/pages/orders/OrderDetailsGrid.jsx
@@ -31,7 +31,7 @@ export function OrderDetailsGrid({ order }) {
             </div>
 
             <div className="product-actions">
-              <a href="tracking.html">
+              <a href="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
