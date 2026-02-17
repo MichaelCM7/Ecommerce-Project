5e144d23f66faa4b20017f883b9d6a5b0b8bfb61
Michael Mbugua <michaelmbugua777@gmail.com>
2026-02-17 23:23:43 +0300

    update path to view properties added to the link

--- frontend/src/pages/orders/OrderDetailsGrid.jsx
+++ frontend/src/pages/orders/OrderDetailsGrid.jsx
@@ -31,7 +31,7 @@ export function OrderDetailsGrid({ order }) {
             </div>
 
             <div className="product-actions">
-              <a href="/tracking">
+              <a href={`/tracking/${order.id}/${order.products.productId}`}>
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
