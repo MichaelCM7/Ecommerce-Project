2b68fe6aa0ffee111f65b75bdedfa1050cbe59a3
Michael Mbugua <michaelmbugua777@gmail.com>
2026-02-18 12:07:39 +0300

    make the progress bar interactive

--- frontend/src/pages/TrackingPage.jsx
+++ frontend/src/pages/TrackingPage.jsx
@@ -23,9 +23,23 @@ export function TrackingPage({ cart }) {
     return null;
   }
 
-  const selectedProduct = order.products.find((product) => {
+  const orderProduct = order.products.find((product) => {
     return productId === product.productId
   });
+
+  const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+
+  let deliveryPercentage = (timePassedMs / totalDeliveryTimeMs) * 100;
+
+  if (deliveryPercentage > 100) {
+    deliveryPercentage = 100;
+  }
+
+  console.log(totalDeliveryTimeMs);
+  console.log(timePassedMs);
+  console.log(deliveryPercentage);
 
   return (
     <>
@@ -34,40 +48,40 @@ export function TrackingPage({ cart }) {
 
       <Header cart={cart} />
 
-      <div class="tracking-page">
-        <div class="order-tracking">
-          <Link class="back-to-orders-link link-primary" to="/orders">
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <Link className="back-to-orders-link link-primary" to="/orders">
             View all orders
           </Link>
 
-          <div class="delivery-date">
-            Arriving on {dayjs(selectedProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
+          <div className="delivery-date">
+            Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
 
-          <div class="product-info">
-            {selectedProduct.product.name}
+          <div className="product-info">
+            {orderProduct.product.name}
           </div>
 
-          <div class="product-info">
-            Quantity: {selectedProduct.quantity}
+          <div className="product-info">
+            Quantity: {orderProduct.quantity}
           </div>
 
-          <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+          <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
 
-          <div class="progress-labels-container">
-            <div class="progress-label">
+          <div className="progress-labels-container">
+            <div className="progress-label">
               Preparing
             </div>
-            <div class="progress-label current-status">
+            <div className="progress-label current-status">
               Shipped
             </div>
-            <div class="progress-label">
+            <div className="progress-label">
               Delivered
             </div>
           </div>
 
-          <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+          <div className="progress-bar-container">
+            <div className="progress-bar" style={{width:`${deliveryPercentage}%`}}></div>
           </div>
         </div>
       </div>
