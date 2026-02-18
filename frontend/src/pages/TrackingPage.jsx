bc820c85525aae3c22c331a64529e958355339c3
Michael Mbugua <michaelmbugua777@gmail.com>
2026-02-18 11:18:30 +0300

    fix issue with object calls and map params to tracking page with accurate data

--- frontend/src/pages/TrackingPage.jsx
+++ frontend/src/pages/TrackingPage.jsx
@@ -1,10 +1,31 @@
 import './TrackingPage.css';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { Link, useParams } from 'react-router';
 import trackingIcon from '../assets/images/tracking-favicon.png';
 
 export function TrackingPage({ cart }) {
   const { orderId, productId } = useParams();
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    const fetchTrackingData = async () => {
+      const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+      setOrder(response.data);
+    }
+
+    fetchTrackingData();
+  }, [orderId]);
+
+  if (!order) {
+    return null;
+  }
+
+  const selectedProduct = order.products.find((product) => {
+    return productId === product.productId
+  });
 
   return (
     <>
@@ -14,42 +35,42 @@ export function TrackingPage({ cart }) {
       <Header cart={cart} />
 
       <div class="tracking-page">
-      <div class="order-tracking">
-        <Link class="back-to-orders-link link-primary" to="/orders">
-          View all orders
-        </Link>
+        <div class="order-tracking">
+          <Link class="back-to-orders-link link-primary" to="/orders">
+            View all orders
+          </Link>
 
-        <div class="delivery-date">
-          Arriving on Monday, June 13
-        </div>
-
-        <div class="product-info">
-          Black and Gray Athletic Cotton Socks - 6 Pairs
-        </div>
-
-        <div class="product-info">
-          Quantity: 1
-        </div>
-
-        <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-
-        <div class="progress-labels-container">
-          <div class="progress-label">
-            Preparing
+          <div class="delivery-date">
+            Arriving on {dayjs(selectedProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
-          <div class="progress-label current-status">
-            Shipped
-          </div>
-          <div class="progress-label">
-            Delivered
-          </div>
-        </div>
 
-        <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="product-info">
+            {selectedProduct.product.name}
+          </div>
+
+          <div class="product-info">
+            Quantity: {selectedProduct.quantity}
+          </div>
+
+          <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+
+          <div class="progress-labels-container">
+            <div class="progress-label">
+              Preparing
+            </div>
+            <div class="progress-label current-status">
+              Shipped
+            </div>
+            <div class="progress-label">
+              Delivered
+            </div>
+          </div>
+
+          <div class="progress-bar-container">
+            <div class="progress-bar"></div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- frontend/src/pages/orders/OrderHeader.jsx
+++ frontend/src/pages/orders/OrderHeader.jsx
@@ -7,7 +7,8 @@ export function OrderHeader({ order }) {
       <div className="order-header-left-section">
         <div className="order-date">
           <div className="order-header-label">Order Placed:</div>
-          <div>{dayjs(order.orderTimeMS).format('MMMM D')}</div>
+          <div>{dayjs(order.orderTimeMs).format('MMMM D')}
+          </div>
         </div>
         <div className="order-total">
           <div className="order-header-label">Total:</div>
